f027184025ec2cacbf6da4801a86b834074fca8d
jose <josemanuel94@gmail.com>
2021-05-24 16:24:14 -0400

    rest_country_api_v1

---
+++ js/js.js
@@ -0,0 +1,200 @@
+//funciones--------------------------------------------------------------------------------------------------------------------------
+//funcion q  carga todos los paises
+const get_all_countries = async ()=>{
+    try{
+        //llama al api y guarda los datos
+        const res = await axios.get("https://restcountries.eu/rest/v2/all" );
+        const array = res.data;
+        
+        //borra los paises
+        $(".country").html("");
+        
+        //guarda los paises en div
+        let div = '';
+        for( let i = 0; i < 30 ; i++){
+
+            div += `<div class="country_card elements_dark_mode elements_light_mode" >
+                            <a target="_blank" href="page.html">
+                            <div class="cont_flag"><img class="flag" src="${array[i]['flag']}"></div>
+                            <div class="country_name">${array[i]['name']}</div>
+                            <div class="stats population">Population: ${array[i]['population']}</div>
+                            <div class="stats reg">Region: ${array[i]['region']}</div>
+                            <div class="stats capital">Capital: ${array[i]['capital']}</div>
+                        </a>
+                    </div>`;
+        }
+        //inserta los paises
+        $(".country").append(div);
+
+        //pongo a la escucha a todos los paises y guardo el selecionado
+       $("a").click(function (e) { 
+           $.cookie( "pais_selected", $(this).find( ".country_name" ).html() );
+       });
+
+       //arreglo bug de cambio de modo
+        if( mode == "light" )
+            $(".country_card").toggleClass("elements_dark_mode");
+    }
+    catch(err){
+        alert( err.message); 
+    }
+}
+
+//funcion q filtra los paises x region
+function filter_region() { 
+
+    $(".region").css( "color", "inherit" );
+    //borra todos los paises
+    $(".country").html("");
+
+    const region = this.value;
+    
+    //si filtra 'all' ejecutamos la funcion de default
+    if( region == 'all' ) return get_all_countries();
+
+    //se ejecuta automaticamente la funcion
+    (async ()=>{
+        try{
+            //llama al api
+            const res = await axios.get("https://restcountries.eu/rest/v2/region/" + region );
+            const array = res.data;
+
+            //guarda los paises en div
+            let div = '';
+            for( let i = 0; i < array.length ; i++){
+
+                div += `<div class="country_card elements_dark_mode elements_light_mode" >
+                            <a target="_blank" href="page.html">
+                                <div class="cont_flag"><img class="flag" src="${array[i]['flag']}"></div>
+                                <div class="country_name">${array[i]['name']}</div>
+                                <div class="stats population">Population: ${array[i]['population']}</div>
+                                <div class="stats reg">Region: ${array[i]['region']}</div>
+                                <div class="stats capital">Capital: ${array[i]['capital']}</div>
+                            </a>
+                        </div>`;
+            }
+            //inserta los paises
+            $(".country").append(div);
+
+            //pongo a la escucha a todos los paises y guardo el selecionado
+            $("a").click(function (e) { 
+                $.cookie( "pais_selected", $(this).find( ".country_name" ).html() );
+            });
+
+            //arreglo bug de cambio de modo
+            if( mode == "light" )
+                $(".country_card").toggleClass("elements_dark_mode");
+        }
+        catch(err){
+            alert( err.message); 
+        }
+    })();
+
+}
+
+
+function search() { 
+
+    //obtego el name
+    const name = this.value;
+
+    //si el 'input' esta vacio ejecutamos la funcion de default
+    if( !name ) return get_all_countries();
+    
+    console.log(name);
+    //borro todos los paises
+    $(".country").html("");
+   
+    //ejecuto  automaticamente esta funcion
+    (async ()=>{
+        try{
+            //llamo al api
+            const res = await axios.get("https://restcountries.eu/rest/v2/name/" + name );
+            const array = res.data;
+            
+            //guarda los paises en div
+            let div = '';
+            for( let i = 0; i < array.length ; i++){
+
+                div += `<div class="country_card elements_dark_mode elements_light_mode" >
+                            <a target="_blank" href="page.html">
+                                <div class="cont_flag"><img class="flag" src="${array[i]['flag']}"></div>
+                                <div class="country_name">${array[i]['name']}</div>
+                                <div class="stats population">Population: ${array[i]['population']}</div>
+                                <div class="stats reg">Region: ${array[i]['region']}</div>
+                                <div class="stats capital">Capital: ${array[i]['capital']}</div>
+                            </a>
+                        </div>`;
+            }
+            //inserta los paises
+            $(".country").append(div);
+
+            //pongo a la escucha a todos los paises y guardo el selecionado
+            $("a").click(function (e) { 
+                $.cookie( "pais_selected", $(this).find( ".country_name" ).html() );
+            });
+
+            //arreglo bug de cambio de modo
+            if( mode == "light" )
+            $(".country_card").toggleClass("elements_dark_mode");
+        }
+        catch(err){
+            alert( "Favor introducir un nombre valido" );
+            //alert( err); 
+        }
+    })();
+}
+
+
+//funcion q ejecuta el cambio de modos(clases dark and light)
+function change_mode() { 
+    $("body").toggleClass("dark_mode");
+    $(".country_card").toggleClass("elements_dark_mode");
+    $(".nav").toggleClass("elements_dark_mode");
+    $(".region").toggleClass("elements_dark_mode");
+    $(".search_and_filter").toggleClass("dark_mode");
+    $(".search").toggleClass("elements_dark_mode");
+    $(".region option").toggleClass("elements_dark_mode");
+    $(".moon").toggleClass("white");
+    
+    //cambia el modo de estilo
+    if( mode == 'dark' ){
+        mode = "light";
+        $.cookie( "modo", "light" );
+    }
+        else{ 
+            mode = "dark";
+            $.cookie( "modo", "dark" );
+        }
+}
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+//almacena el modo de estilo
+var mode = "dark",
+    mode_change = $.cookie( "modo" );
+
+    if(  mode_change != mode )
+        change_mode( );
+    
+//muestra todos los paises
+get_all_countries();
+
+//cambia entre los dif modos 
+$(".mode").click( change_mode );
+
+
+//filtra los paises x regiones
+$(".region").change( filter_region );
+
+
+// //busca los paises por nombre
+ $(".search").on( "input", search);
+ 
+//arreglo bug de search para q muestre en el filter 'ALL' cuando se busca un pais
+$(".search").keyup(function (e) { 
+   console.log( this.value );
+    if( this.value != "" ){
+        $(".region").val( "all" );
+    }    
+});
---
+++ js/page.js
@@ -0,0 +1,100 @@
+// const { default: axios } = require("axios");
+
+//funcion q ejecuta el cambio de modos(clases dark and light)
+function change_mode() { 
+    $("body").toggleClass("dark_mode");
+    $(".country_card").toggleClass("elements_dark_mode");
+    $(".nav").toggleClass("elements_dark_mode");
+    $(".region").toggleClass("elements_dark_mode");
+    $(".search_and_filter").toggleClass("dark_mode");
+    $(".search").toggleClass("elements_dark_mode");
+    $(".region option").toggleClass("elements_dark_mode");
+    $(".btn").toggleClass("elements_dark_mode");
+    $(".border_stats span a").toggleClass("elements_dark_mode text");
+    $(".stats_details span").toggleClass("text");
+    $(".moon").toggleClass("white");
+
+    
+    //cambia el modo de estilo
+    if( mode == 'dark' ){
+        mode = "light";
+        $.cookie( "modo", "light" );
+    }
+        else{ 
+            mode = "dark";
+            $.cookie( "modo", "dark" );
+        }
+}
+
+
+//coloco los datos stats  del pais seleccionado (arrays)
+const pon_datos_stats = ( datos, direccion)=>{
+    for( let i = 0; i< datos.length; i++ )
+        if( datos[i]['name'] == undefined ){
+            $(direccion).append( ' ' + datos[i] );
+            if( i+1 < datos.length )
+                $(direccion).append( ',' );
+        }
+        else{
+            $(direccion).append( ' ' + datos[i]['name'] );
+            if( i+1 < datos.length )
+                $(direccion).append( ',' );
+        }
+};
+
+//coloco los datos border del pais seleccionado (arrays) y llamo a la api para poner el nombre completo porque me lo dan en codigo(ej 'FRA' )
+const pon_datos_border = async ( data, direccion )=>{
+    
+    for( let i=0; i< data.length; i++ ){
+        const country_name = await axios.get( "https://restcountries.eu/rest/v2/alpha/" + data[i] );
+        if( mode == "light" )
+        $( direccion ).append( `<a href= "page.html" target="_self" class=" text elements_light_mode">` + country_name.data.name  + '</a>');
+            else
+            $( direccion ).append( `<a href= "page.html" target="_self" class="elements_dark_mode elements_light_mode">` + country_name.data.name  + '</a>');
+        console.log(country_name.data.name );
+        
+    }
+    $(".border_stats a").click(function (e) { 
+        $.cookie( "pais_selected", $(this).html() );    
+    });
+}
+
+//almacena el modo de estilo
+var mode = "dark",
+    mode_change = $.cookie( "modo" );
+
+    if(  mode_change != mode )
+        change_mode( );
+
+//cambia entre los dif modos 
+$(".mode").click( change_mode );
+
+//accedemos a la cookie guardada
+var pais_selected = $.cookie( "pais_selected" );
+
+//llamada a el pais seleccionado
+axios.get( "https://restcountries.eu/rest/v2/name/" + pais_selected )
+.then( (res)=>{
+
+    var array = res.data[0];
+    console.log( array );
+
+    //coloco los datos del pais seleccionado
+    $(".img_preview").attr("src", array["flag"]);
+    $(".country_name_details").html(array['name']);
+    $(".native_stats span").html(  array['nativeName']);
+    $(".population_stats span").html(  array['population']);
+    $(".region_stats span").html(  array['region']);
+    $(".capital span").html(  array['capital']);    
+    $(".sub_stats span").html( array['subregion']);
+
+    //pongo los datos q tienen arrays
+    pon_datos_stats( array['languages'], ".languages span" );
+    pon_datos_stats( array.currencies, ".currencies_stats span" );
+    pon_datos_stats( array['topLevelDomain'], ".top_stats span" );
+    
+    //pongo los datos de los paises border
+    pon_datos_border( array.borders, ".border_stats span"); 
+
+});
+
